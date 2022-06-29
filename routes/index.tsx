4993ac9d2be6bc84346ee7cbca182eb7b94f2903
Kyle Tolle <kyle@nullsix.com>
2022-06-29 17:18:14 +0000

    Add initial random color fetching

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,31 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import Counter from "../islands/Counter.tsx";
 
-export default function Home() {
+interface RandomColor {
+  color: string;
+}
+
+export const handler: Handlers<RandomColor> = {
+  async GET(_, ctx) {
+    const randomColorPayload = await fetch('https://kyletolle-random-color-api.deno.dev/');
+    if (randomColorPayload.status !== 200) {
+      return ctx.render({color: '#FFF'});
+    }
+
+    const randomColor = await randomColorPayload.text();
+    console.info('randomColor', randomColor);
+    return ctx.render({ color: randomColor });
+  }
+}
+
+export default function Home({ data }: PageProps<RandomColor>) {
+  if (!data.color) {
+    return <h1>Fetching color...</h1>;
+  }
+
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
@@ -12,10 +34,10 @@ export default function Home() {
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+        Welcome to random_color_viewer!
       </p>
       <Counter start={3} />
+      <div style={{height: '20px', width: '20px', backgroundColor: data.color}}></div>
     </div>
   );
 }
